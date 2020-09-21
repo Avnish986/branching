1b0331fa0a49021835a19b82dc5b7d55cd4929a5
Avnish986 <56001171+Avnish986@users.noreply.github.com>
2020-09-21 21:47:58 +0530

    changes in line 2

--- src/helloBranching/HelloBranching.java
+++ src/helloBranching/HelloBranching.java
@@ -5,6 +5,7 @@ public class HelloBranching {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("changes 1");
+			System.out.println("changes 2");
 	}
 
 }
